0b262a51d0052f9a37bed18ec960f505a7ea2297
Juan Searle <jsearle@gmail.com>
2017-04-20 09:56:53 +0200

    New class for bouncing uibuttons

---
+++ Hula/Application/HLBouncingButton.swift
@@ -0,0 +1,31 @@
+//
+//  HLBouncingButton.swift
+//  Hula
+//
+//  Created by Juan Searle on 20/04/2017.
+//  Copyright © 2017 star. All rights reserved.
+//
+
+import UIKit
+
+class HLBouncingButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+     */
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.transform = CGAffineTransform(scaleX: 1.1,y: 1.1);
+        })
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.transform = CGAffineTransform(scaleX: 1,y: 1);
+        })
+    }
+
+}
