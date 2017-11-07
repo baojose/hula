3bb28531829b0e73892d77a753ba17e0ff8495b5
Juan Searle <jsearle@gmail.com>
2017-11-07 23:23:16 +0100

    Layout fixes

--- Hula/Application/Views/Main/HLDashboardViewController.swift
+++ Hula/Application/Views/Main/HLDashboardViewController.swift
@@ -52,7 +52,7 @@ class HLDashboardViewController: BaseViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        self.mainCollectionView.frame = self.view.frame
+        //self.mainCollectionView.frame = self.view.frame
         self.mainCollectionView.scrollToItem(at: IndexPath(row: 0, section: 0) , at: .top, animated: true)
         self.mainCollectionView.collectionViewLayout.invalidateLayout()
         
